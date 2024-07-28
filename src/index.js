11d1355bb7e27a13e5635992c41904fdee211982
Prajyot Patil <prajyotbpatil2744@gmail.com>
2024-07-28 10:33:57 +0530

    utils add

--- src/index.js
+++ src/index.js
@@ -8,3 +8,12 @@ dotenv.config({
 
 
 connectDB()
+
+.then(() =>{
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(` surver is running at port : ${process.env.PORT}`)
+    })
+})
+.catch((err) => {
+    console.log(`MONGODB connection failed !!!`,err);
+})
